d47b04aa4ede0b7ba5c1f4d6bb86c22867c7fc95
Yui Kondo <yuikondobook@gmail.com>
2021-07-03 17:14:31 +0900

    create articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,48 @@
 class ApplicationController < ActionController::Base
+  # Read
+  # See all articles
+  def index
+      @articles = Article.all
+  end
+  # See details about one article
+  def show
+      @article = Article.find(params[:id])
+  end
+  # Create a restaurant
+  def new
+      @article = Article.new # needed to instantiate the form_for
+  end
+
+  def create
+      @article = Article.new(article_params) 
+      # DIFF taks_params & params[:article]
+      @article.save
+      # Will raise ActiveModel::ForbiddenAttributesError
+      redirect_to article_path(@article)
+  end
+
+  # Update a restaurant
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      redirect_to article_path(@article)
+  end
+  # Destroy a restaurant
+
+  def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+      params.require(:article).permit(:title, :content)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new(
+    title: Faker::Quote.famous_last_words,
+    content: Faker::TvShows::BigBangTheory.quote
+  )
+  article.save!
+end
